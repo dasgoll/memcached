ddcca99a8c3a95bd902f96759263c6391288ee88
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-08-19 14:22:49 +0900

    fix FC019

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-case node[:platform]
+case node["platform"]
 when "centos"
   if node["platform"] =="centos" && node["platform_version"][0] == "5"
     include_recipe "yum::remi"
